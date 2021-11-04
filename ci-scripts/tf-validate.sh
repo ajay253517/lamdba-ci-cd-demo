bc03e0b100eda384226025c9c8b3662ca68affe6
ajay253517 <ajay.baljoshi@gmail.com>
2021-11-04 19:04:00 +0530

    Added tf validate script

--- ci-scripts/tf-validate.sh
+++ ci-scripts/tf-validate.sh
@@ -4,6 +4,6 @@ CO_ID=$1
 fList=$(git diff-tree --no-commit-id --name-only -r $CO_ID | grep "\.tf")
 [ -z "$fList" ] && echo "no tf files modified" && exit 0
 
-get_dir=$(git diff --name-only HEAD~1 | awk -F "/*[^/]*/*$" '{ print ($1 == "" ? "." : $1); }' | sort | uniq | grep -v github)
+get_dir=$(git diff --name-only HEAD~1 | awk -F "/*[^/]*/*$" '{ print ($1 == "" ? "." : $1); }' | sort | uniq | grep -v github  | grep "tf-*")
 
 echo $get_dir
